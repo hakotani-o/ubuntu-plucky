1eb1feac5692fb3acf8375f68177102d8d0f74de
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2025-11-02 21:11:41 +0900

    Add files via upload

---
+++ nconfig.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -f .config
+cat arch/arm64/configs/defconfig ../../overlay/my-add.txt > .config
+#cat ../../overlay/my-add.txt arch/arm64/configs/defconfig ../../overlay/my-add.txt > .config
+#cat arch/arm64/configs/defconfig ../../overlay/my-add.txt ../../overlay/hd-audio-config > .config
+
+{
+	sleep 15
+	echo '6'
+	sleep 5
+	echo "\n"
+	sleep 5
+	echo "\n"
+	echo '9'
+	echo "\n"
+} | make nconfig
+
+	cp .config ../../overlay/1-config.txt
