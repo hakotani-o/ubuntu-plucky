ad63bd085c4243bd035f1e77301377b1ccb4cce6
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2025-11-02 21:12:46 +0900

    Update nconfig.sh to use my-add.txt directly

--- nconfig.sh
+++ nconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f .config
-cat arch/arm64/configs/defconfig ../../overlay/my-add.txt > .config
+cat arch/arm64/configs/defconfig ../../my-add.txt > .config
 #cat ../../overlay/my-add.txt arch/arm64/configs/defconfig ../../overlay/my-add.txt > .config
 #cat arch/arm64/configs/defconfig ../../overlay/my-add.txt ../../overlay/hd-audio-config > .config
 
