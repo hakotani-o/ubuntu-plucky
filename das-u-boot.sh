61fc89138f2f76847988afc9bf4781d8557e0b12
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2025-11-02 17:25:31 +0900

    Add files via upload

--- das-u-boot.sh
+++ das-u-boot.sh
@@ -18,30 +18,33 @@ fi
 	cd arm64
 
 		git clone --depth 1 https://github.com/rockchip-linux/rkbin
-		DDR=`ls rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2400MHz*.bin`
+		DDR=`ls rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v*.bin`
 		BL31=`ls rkbin/bin/rk35/rk3588_bl31*.elf`
+	export BL31=`pwd`/$BL31
+	export ROCKCHIP_TPL=`pwd`/$DDR
 echo ""
-echo "export ROCKCHIP_TPL=../$DDR"
-echo "export BL31=../$BL31"
+echo "export ROCKCHIP_TPL=$ROCKCHIP_TPL"
+echo "export BL31=$BL31"
+echo ""
+		export PATH=`pwd`/tools/binman:$PATH
+echo ""
+echo "PATH=$PATH"
 echo ""
 
-		git clone --depth 1 https://github.com/u-boot/u-boot.git -b v2025.$2
+		git clone --depth 1 https://gitlab.com/u-boot/u-boot.git -b v2025.$2
 		cd u-boot
 		if [ ! -f configs/$1 ]; then
 			echo "$1 not found in configs"
 			cd ..
 			exit 1
 		fi
-	export BL31=../$BL31
-	export ROCKCHIP_TPL="../$DDR"
 
-	sed -i 's/scsi //' include/configs/rockchip-common.h
-	sed -i 's/mmc1/scsi mmc1/' include/configs/rockchip-common.h
-	sed -i 's/#ifndef CONFIG_XPL_BUILD/#ifndef CONFIG_XPL_BUILD\n\n# define BOOT_TARGET_DEVICES_SCSI(func)	func(SCSI, scsi, 0, 0, 1) func(SCSI, scsi, 0, 0, 2) func(SCSI, scsi, 0, 0, 3)/' include/configs/rockchip-common.h
+	echo 'CONFIG_SYS_SOC="rk3588"' >> configs/$1
+sed -i 's/#ifndef CONFIG_XPL_BUILD/#ifndef CONFIG_XPL_BUILD\n\n# define BOOT_TARGET_DEVICES_SCSI(func)	func(SCSI, scsi, 0, 0, 0) func(SCSI, scsi, 0, 0, 1)\n\n# define BOOT_TARGET_DEVICES_NVME(func)  func(NVME, nvme, 0, 0, 0) func(NVME, nvme, 0, 0, 1)\n\n/' include/configs/rockchip-common.h
 
-		make $1
-		make
-		cp u-boot-rockchip.bin ../..
+		make clean $1
+		make -j8
+		cp u-boot-rockchip.bin ../../overlay/
 	echo "dd if=u-boot-rockchip.bin of=/dev/sdX seek=1 bs=32k conv=fsync"
 	cd ../..
 echo "DISK usage"
