35e622b2de8fc7d45a247ae300f2e444226f1112
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-04-24 22:11:27 +0900

    Update device tree and firmware paths in script

--- disk_image.sh
+++ disk_image.sh
@@ -118,12 +118,12 @@ mkdir -p ${mount_point}
 
 # Copy the rootfs to root partition
 tar -xpf "${rootfs}" -C ${mount_point}/writable
-fdt_name="/rockchip/$3.dtb"
+fdt_name="device-tree/rockchip/$3.dtb"
 
-dtbs_install_path="/usr/lib/linux-image-"
+dtbs_install_path="/lib/firmware/"
 
-if [ ! -f ${mount_point}/writable${dtbs_install_path}${kernel_version}${fdt_name} ]; then
-	echo "${dtbs_install_path}${kernel_version}${fdt_name}"
+if [ ! -f ${mount_point}/writable${dtbs_install_path}${kernel_version}/${fdt_name} ]; then
+	echo "${dtbs_install_path}${kernel_version}/${fdt_name}"
 	echo "$3.dtb not found"
 	exit 1
 fi
