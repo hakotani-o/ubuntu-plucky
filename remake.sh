fa997146046956005c447f5fbd50460738208a47
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-04-24 22:07:11 +0900

    Modify tmpfs size and update file paths in remake.sh
    Updated memory size for tmpfs mount and changed file references.

--- remake.sh
+++ remake.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 set -eE
 trap 'echo Error: in $0 on line $LINENO' ERR
@@ -13,17 +13,17 @@ linux_dir=$1
 rm -rf $linux_dir && mkdir $linux_dir
 mem_size=`free --giga|grep Mem|awk '{print $2}'`
 if [ $mem_size -gt 4 ]; then
-	sudo mount -t tmpfs -o size=8G tmpfs $linux_dir
+	sudo mount -t tmpfs -o size=4G tmpfs $linux_dir
 fi
 
 cd $linux_dir
 
-if [ ! -f ../overlay/tmp_var.txt ]; then
-	echo "../overlay/tmp_var.txt not found"
+if [ ! -f ../tmp_var.txt ]; then
+	echo "../tmp_var.txt not found"
 	exit 1
 fi
 
-. ../overlay/tmp_var.txt
+. ../tmp_var.txt
 secnd=`echo $tmp_var | sed 's/rockchip//'`
 echo "secnd=$secnd"
 secnd="${secnd}1_arm64"
@@ -31,7 +31,7 @@ echo "secnd=$secnd"
 
 # 解凍
 mkdir -p temp_deb
-cp ../kernel/linux-image-${tmp_var}_${secnd}.deb .
+cp ../linux-image-${tmp_var}_${secnd}.deb .
 dpkg-deb -x linux-image-${tmp_var}_${secnd}.deb temp_deb
 dpkg-deb -e linux-image-${tmp_var}_${secnd}.deb temp_deb/DEBIAN
 
@@ -42,11 +42,9 @@ mv temp_deb/usr/lib/linux-image-$tmp_var/* "$NEW_PATH"
 rm -rf temp_deb/usr/lib/linux-image-${tmp_var}
 
 # 再パッケージ
-#dpkg-deb -b temp_deb linux-image-${tmp_var}-custom.deb
-#cp linux-image-${tmp_var}-custom.deb ../kernel
 rm linux-image-${tmp_var}_${secnd}.deb
 dpkg-deb -b temp_deb linux-image-${tmp_var}_${secnd}.deb
-cp linux-image-${tmp_var}_${secnd}.deb ../kernel
+cp linux-image-${tmp_var}_${secnd}.deb ..
 
 
 # Exit trap is no longer needed
